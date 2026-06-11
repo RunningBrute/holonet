f8bb2b991fb5022e7a1973acf86282da94c9107a
RunningBrute <piotrekbas8@gmail.com>
2026-06-12 00:15:42 +0200

    create SessionMAnager class

--- app/main.py
+++ app/main.py
@@ -6,8 +6,39 @@ from app.models.update_code_request import UpdateCodeRequest
 
 app = FastAPI()
 
+class SessionManager:
+    def __init__(self):
+        self.sessions: dict [str, Session] = {}
 
-sessions: dict[str, Session] = {}
+    def create_session(self) -> Session:
+        session = Session(
+            id=str(uuid.uuid4()),
+            code=""
+        )
+        self.sessions[session.id] = session
+        return session
+
+
+    def get_session(self, id: str) -> Session:
+        session = self.sessions.get(id)
+
+        if session is None:
+            raise HTTPException(
+                status_code=404,
+                detail="Session not found"
+            )
+
+        return session
+
+
+    def update_code(self, id: str, code: str) -> Session:
+        session = self.get_session(id)
+        session.code = code
+        return session
+
+
+session_manager = SessionManager()
+
 
 @app.get("/")
 def root():
@@ -19,36 +50,13 @@ def health():
 
 @app.post("/sessions")
 def create_session():
-    session = Session(
-        id=str(uuid.uuid4()),
-        code=""
-    )
-    sessions[session.id] = session
-
+    session = session_manager.create_session()
     return {"id": session.id}
 
 @app.get("/sessions/{id}")
 def get_session(id: str):
-    session = sessions.get(id)
-
-    if session is None:
-        raise HTTPException(
-            status_code=404,
-            detail="Session not found"
-        )
-
-    return session
+    return session_manager.get_session(id)
 
 @app.put("/sessions/{id}/code")
 def update_code(id: str, request: UpdateCodeRequest):
-    session = sessions.get(id)
-
-    if session is None:
-        raise HTTPException(
-            status_code=404,
-            detail="Session not found"
-        )
-    
-    session.code = request.code
-
-    return session
+    return session_manager.update_code(id, request.code)
